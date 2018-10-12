00f7add2982147438d30da6fc7e10ab7899e4596
Paul Linck <paul.linck@gmail.com>
2018-10-12 15:30:59 -0400

    Part 1 of Building a Shopping List Application With CloudKit: Introduction https://code.tutsplus.com/tutorials/building-a-shopping-list-application-with-cloudkit-introduction--cms-24674

--- Lists/ViewController.swift
+++ Lists/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import CloudKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fetchUserRecordID()
     }
 
+    private func fetchUserRecordID () {
+        // Fetch Default Container
+        let defaultContainer = CKContainer.default()
+        
+        // Fetch User Record
+        defaultContainer.fetchUserRecordID {
+            (recordID, error) -> Void in
+            if let responseError = error {
+                // error isnt nil so we had a problem
+                print(responseError);
+            } else if let userRecordID = recordID {
+                // valid recordID so send to main queue
+                DispatchQueue.main.sync {
+                    self.fetchUserRecord(recordID: userRecordID)
+                }  // queue
+            } // else if
+        } // fetchUSerRecordID
+    } // func fetchUserRecordID
 
-}
+    private func fetchUserRecord(recordID: CKRecord.ID) {
+        // Fetch Default Container
+        let defaultContainer = CKContainer.default()
+        
+        // Fetch Private Database
+        let privateDatabase = defaultContainer.privateCloudDatabase
+        
+        // Fetch User Record
+        privateDatabase.fetch(withRecordID: recordID) {  //Note trailing closure syntax
+            (record, error) -> Void in
+            if let responseError = error {
+                print(responseError)
+            } else if let userRecord = record {
+                print(userRecord)
+            }
+            
+        }
+        
+    }
+    
+} // class
 
